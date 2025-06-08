6c80cff12094e250d9778144655a08699f8de7f7
Julie <julie.habbestad@hotmail.com>
2025-06-08 21:04:13 +0200

    update image url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,9 +2,9 @@ export const projects = [
   {
     id: "javascript-framework",
     title: "Javascript framework",
-    description: "A ecommerce shop. Whatever you need. We have it. From essentials to indulgences, shop it all in one place—hassle-free, fast, and at the best prices. Its a general store",
+    description: "A ecommerce shop. Whatever you need. We have it. From essentials to indulgences, shop it all in one place—hassle-free, fast, and at the best prices. Its a general store. here are more things I would like to change in the future when i have more time. Like using array indices as React keys, using const instead of let when variables are not reassigned. Also the design of the site could be improved.",
     technologies: ["React", "Styled Components"],
-    image: "/my-portfolio/images/javascript-framework.png", 
+    image: "/my-portfolio/images/javascript-framewor.jpeg", 
     link: "https://generalstoreblob.netlify.app/",
     readme: "https://github.com/juliehabb/ecommerce-app/blob/master/README.md"
   },
@@ -13,7 +13,7 @@ export const projects = [
     title: "Semester project",
     description: "A auction site where one can bid and sell own products. I improved the site by adding UI feedback in the form of a modal instead of alerts. Overall the site could use a bit of shining up visually and a bit more addons functionality wise. But I'm happy with the base code. ",
     technologies: ["React", "Bootstrap"],
-    image: "/my-portfolio/images/Semester-project-2.png",
+    image: "/my-portfolio/images/Semester-project-2.jpeg",
     link: "https://juliehabb.github.io/auction-website/?classId=bbc516b1-3b66-40d2-beb1-b696fd0c5230&assignmentId=c181e851-cc93-4473-8eaf-98528a341b16&submissionId=05ee203e-db6c-6ec7-605c-50cd8ab4dbc3",
     readme: "https://github.com/juliehabb/auction-website/blob/main/README.md"
   },
@@ -22,7 +22,7 @@ export const projects = [
     title: "exam project 2",
     description: "Final exam. A accomodation booking site named Holidaze. One can book accomodations and list own accomodations for others too book. Would like to add animations and improve design of website. ",
     technologies: ["React", "Tailwind CSS"],
-    image: "/my-portfolio/images/exam-project-2.png",
+    image: "/my-portfolio/images/exam-project-2.jpeg",
     link: "juliehabb.github.io/final-exam/",
     readme: "https://github.com/juliehabb/final-exam/blob/master/README.md"
   }
