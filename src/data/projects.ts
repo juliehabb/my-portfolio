f68f310aeb25c0070e30acab2b6433b7c172c2d1
Julie <julie.habbestad@hotmail.com>
2025-06-08 19:21:32 +0200

    updated image links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,17 +2,17 @@ export const projects = [
   {
     id: "javascript-framework",
     title: "Javascript framework",
-    description: "https://github.com/juliehabb/ecommerce-app. E-commerce store. ",
+    description: "A ecommerce shop. Whatever you need. We have it. From essentials to indulgences, shop it all in one place—hassle-free, fast, and at the best prices. ",
     technologies: ["React", "Tailwind CSS", "TypeScript", "REST API"],
-    image: "../public/images/javascript-framework.png", 
+    image: "/my-portfolio/images/javascript-framework.png", 
     link: "https://generalstoreblob.netlify.app/",
   },
   {
     id: "semester-project",
     title: "Semester project",
     description: "My personal portfolio showcasing projects and skills.",
-    technologies: ["React", "Tailwind CSS", "Vite"],
-    image: "../public/images/Semester-project-2.png",
+    technologies: ["React", "Tailwind CSS"],
+    image: "/my-portfolio/images/Semester-project-2.png",
     link: "https://juliehabb.github.io/auction-website/?classId=bbc516b1-3b66-40d2-beb1-b696fd0c5230&assignmentId=c181e851-cc93-4473-8eaf-98528a341b16&submissionId=05ee203e-db6c-6ec7-605c-50cd8ab4dbc3",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     title: "exam project 2",
     description: "My personal portfolio showcasing projects and skills.",
     technologies: ["React", "Tailwind CSS", "Vite"],
-    image: "../public/images/exam-project-2.png",
+    image: "/my-portfolio/images/exam-project-2.png",
     link: "juliehabb.github.io/final-exam/",
   }
 ];
